ab815b1cdec8d37d52ab3803065dd01154beb697
Zeus-Eternal <121900625+Zeus-Eternal@users.noreply.github.com>
2025-06-25 23:17:40 -0400

    Switch GTK app to GTK4

--- gtk_app/main.py
+++ gtk_app/main.py
@@ -1,16 +1,15 @@
 import asyncio
-from gi.repository import Gtk, GLib
 import gi
 import aiohttp
 
-gi.require_version('Gtk', '3.0')  # Change to '4.0' if you're using GTK 4
+gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk, GLib
 API_URL = "http://localhost:8000/api/networks?limit=50"
 
 
-class NetworkWindow(Gtk.Window):
-    def __init__(self):
-        super().__init__(title="ZeusNet GTK")
+class NetworkWindow(Gtk.ApplicationWindow):
+    def __init__(self, app: Gtk.Application):
+        super().__init__(application=app, title="ZeusNet GTK")
         self.set_default_size(400, 300)
 
         self.liststore = Gtk.ListStore(str, int)
@@ -21,18 +20,12 @@ class NetworkWindow(Gtk.Window):
         renderer_rssi = Gtk.CellRendererText()
         column_rssi = Gtk.TreeViewColumn("RSSI", renderer_rssi, text=1)
         treeview.append_column(column_rssi)
-        scrolled = Gtk.ScrolledWindow()
-        scrolled.add(treeview)
-        self.add(scrolled)
 
-        GLib.timeout_add_seconds(5, self.refresh)
-        # Scrollable window
         scrolled = Gtk.ScrolledWindow()
         scrolled.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
-        scrolled.add(treeview)
-        self.add(scrolled)
+        scrolled.set_child(treeview)
+        self.set_child(scrolled)
 
-        # Refresh every 5 seconds
         GLib.timeout_add_seconds(5, self.refresh)
 
         # First fetch on load
@@ -42,14 +35,6 @@ class NetworkWindow(Gtk.Window):
         try:
             async with aiohttp.ClientSession() as session:
                 async with session.get(API_URL) as resp:
-                    data = await resp.json()
-                    self.liststore.clear()
-                    for item in data:
-                        self.liststore.append(
-                            [item.get("ssid", ""), item.get("rssi", 0)]
-                        )
-        except Exception as e:
-            print("Error fetching networks", e)
                     if resp.status == 200:
                         data = await resp.json()
                         self.liststore.clear()
@@ -67,14 +52,18 @@ class NetworkWindow(Gtk.Window):
         return True
 
 
+class ZeusApp(Gtk.Application):
+    def __init__(self):
+        super().__init__(application_id="com.zeusnet.viewer")
+
+    def do_activate(self, *args):
+        win = NetworkWindow(self)
+        win.present()
+
+
 def main():
-    loop = asyncio.get_event_loop()
-    win = NetworkWindow()
-    win.connect("destroy", Gtk.main_quit)
-    win.show_all()
-    Gtk.main()
-    loop.stop()
-    loop.close()
+    app = ZeusApp()
+    app.run()
 
 
 if __name__ == "__main__":
